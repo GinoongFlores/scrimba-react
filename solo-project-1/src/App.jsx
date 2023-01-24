74008b6226463973e57daa9da4b9f459c1debc8b
ginoongflores <florespaul161@gmail.com>
2023-01-24 17:21:17 +0800

    established templates

--- solo-project-1/src/App.jsx
+++ solo-project-1/src/App.jsx
@@ -1,8 +1,19 @@
 
+import Info from "./components/Info"
+import About from "./components/About"
+import Interests from "./components/Interests"
+import Footer from "./components/Footer"
+
 export default function App() {
+    return (
+        <>
+            <div className="container">
+                <Info />
+                <About />
+                <Interests />
+                <Footer />
+            </div>
+        </>
+    )
 
 }
-
-// const Test = () => {
-//     <App /> 
-// }
---
+++ solo-project-1/src/components/Info.jsx
@@ -0,0 +1,9 @@
+
+export default function Info() {
+    return (
+        <>
+            <h1>This is an info</h1>
+        </>
+    )
+
+}
