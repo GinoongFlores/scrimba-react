e80d304ff77f666cc9423635c8710926b16cb065
ginoongflores <florespaul161@gmail.com>
2023-04-05 18:27:44 +0800

    fixes(styles & navbar)

--- airbnb-clone/src/components/Navbar.jsx
+++ airbnb-clone/src/components/Navbar.jsx
@@ -4,11 +4,9 @@ import logo from "../assets/img/airbnb-logo.png";
 
 const Navbar = () => {
 	return (
-		<div className="navbar">
-			<div className="logo">
-				<img src={logo} alt="logo" className="logo-img" />
-			</div>
-		</div>
+		<nav>
+			<img src={logo} alt="logo" className="nav--logo" />
+		</nav>
 	);
 };
 
