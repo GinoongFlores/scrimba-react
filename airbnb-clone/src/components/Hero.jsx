c63ad8ea26d1ad6f40a342b79862c1fc0cd49430
ginoongflores <florespaul161@gmail.com>
2023-04-05 19:10:31 +0800

    initialize hero section

---
+++ airbnb-clone/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style.css";
+import HeroImage from "../assets/img/photo-grid.png";
+
+const Hero = () => {
+	return (
+		<section className="hero--container">
+			<img src={HeroImage} className="hero--image" alt="hero-image" />
+
+			<div className="hero--description">
+				<h1>Online Experiences</h1>
+				<p>
+					Join unique interactive activities led by one-of-a-kind hosts—all
+					without leaving home.
+				</p>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
