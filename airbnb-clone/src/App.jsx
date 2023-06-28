e5284dc9e66b0d9a2998a05f5e340dc3ccf1c5fe
ginoongflores <florespaul161@gmail.com>
2023-06-28 18:08:01 +0800

    feat(App): add Card comp.

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,12 +1,22 @@
 import "./style.css";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Hero />
+			{/*<Hero />*/}
+			<Card
+				img={"katie-zaferes.png"}
+				rating={"5.0"}
+				reviewCount={6}
+				country={"USA"}
+				title={"Life lessons with Katie Zaferes"}
+				price={136}
+			/>
 		</div>
 	);
 }
