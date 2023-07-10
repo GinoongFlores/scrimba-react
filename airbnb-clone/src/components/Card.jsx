56e82aeddc5d0becded2c6921d4f4060e53a3a93
ginoongflores <florespaul161@gmail.com>
2023-07-10 10:56:28 +0800

    feat(Card): add "item" prop
    Add the "item" prop that consists of objects from the data file.

--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -3,26 +3,36 @@ import "../style.css";
 import CardStar from "../assets/img/star.png";
 
 const Card = (props) => {
-	// console.log(props);
+	console.log(props);
+
+	let badgeText;
+	if (props.item.openSpots === 0) {
+		badgeText = "SOLD OUT";
+	} else if (props.item.location === "Online") {
+		badgeText = "ONLINE";
+	}
 	return (
 		<div className="card--container">
-			{/* this is string interpolation where an expression
+			{/* If the openSpots prop is equal to zero then we will show the "sold out" badge */}
+			{/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+			{badgeText && <div className="card--badge">{badgeText}</div>}
+			{/* On <img> this is string interpolation where an expression
 			is inserted into a string or literal
 			 */}
 			<img
-				src={`src/assets/img/${props.img}`}
+				src={`src/assets/img/${props.item.coverImg}`}
 				alt="katie zaferes"
 				className="card--image"
 			/>
 			<div className="card--stats">
 				<img src={CardStar} alt="star" className="card--star" />
-				<span>{props.rating}</span>
-				<span className="gray">({props.reviewCount}) • </span>
-				<span className="gray">{props.country} </span>
+				<span>{props.item.stats.rating}</span>
+				<span className="gray">({props.item.stats.reviewCount}) • </span>
+				<span className="gray">{props.item.location} </span>
 			</div>
-			<p className="card--title">{props.title}</p>
+			<p className="card--title">{props.item.title}</p>
 			<p className="card--price">
-				<span className="bold"> From ${props.price} </span> / person
+				<span className="bold"> From ${props.item.price} </span> / person
 			</p>
 		</div>
 	);
