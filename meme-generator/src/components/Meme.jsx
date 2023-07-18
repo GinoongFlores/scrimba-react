afadada9cf6795bac8a0ae279945277885fa41c6
ginoongflores <florespaul161@gmail.com>
2023-07-18 15:03:45 +0800

    feat(meme-generator): updated)

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,16 +1,29 @@
 import "../index.css";
+import memesData from "../memesData";
+
+// console.log(memesData);
 
 const Meme = () => {
+	// console.log(memesData.data.memes.url);
+
+	function buttonClick() {
+		const randomeMemeImg =
+			memesData.data.memes[
+				Math.floor(Math.random() * memesData.data.memes.length)
+			].url;
+		console.log(randomeMemeImg);
+	}
+
 	return (
 		<main>
 			<form className="form">
 				<input type="text" placeholder="Top Text" className="form--input" />
 				<input type="text" placeholder="Bottom Text" className="form--input" />
-				<button type="button" className="form--button">
+				<button type="button" onClick={buttonClick} className="form--button">
 					Get a new meme image 🖼
 				</button>
+				<img src="/img/memeimg.png" alt="image meme" className="meme-img" />
 			</form>
-			<img src="/img/memeimg.png" alt="image meme" className="meme-img" />
 		</main>
 	);
 };
