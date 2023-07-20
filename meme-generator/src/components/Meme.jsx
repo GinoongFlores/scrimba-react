46a3f4d429aa9950fe8a7dd50e8b24a3b7f7ddea
GinoongFlores <florespaul161@gmail.com>
2023-07-20 19:25:53 +0800

    feat(Meme): add useState hook & randomize images
    Adds a useState hook to get the random image

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,17 +1,28 @@
 import "../index.css";
+import React from "react";
 import memesData from "../memesData";
 
 // console.log(memesData);
 
 const Meme = () => {
-	// console.log(memesData.data.memes.url);
+	const [memeImg, setMemeImg] = React.useState("");
 
-	function buttonClick() {
-		const randomeMemeImg =
-			memesData.data.memes[
-				Math.floor(Math.random() * memesData.data.memes.length)
-			].url;
-		console.log(randomeMemeImg);
+	// Inline way to do it
+	// const randomeMemeImg =
+	// 	memesData.data.memes[
+	// 		Math.floor(Math.random() * memesData.data.memes.length)
+	// 	].url;
+
+	// Another way to do it to make it more readable
+	const memesArray = memesData.data.memes; // array of memes
+	const randomNumber = Math.floor(Math.random() * memesArray.length) + 1; // random number between 1 and 100
+	let url = memesArray[randomNumber].url; // url of the meme at the random position
+
+	function getMemeImg() {
+		// we can decide not to use a callback function here because we will not be using the previous state of the `memeImg` state which is the empty string.
+		setMemeImg(url);
+
+		console.log(memeImg);
 	}
 
 	return (
@@ -19,10 +30,10 @@ const Meme = () => {
 			<form className="form">
 				<input type="text" placeholder="Top Text" className="form--input" />
 				<input type="text" placeholder="Bottom Text" className="form--input" />
-				<button type="button" onClick={buttonClick} className="form--button">
+				<button type="button" onClick={getMemeImg} className="form--button">
 					Get a new meme image 🖼
 				</button>
-				<img src="/img/memeimg.png" alt="image meme" className="meme-img" />
+				<img src={memeImg} alt="image meme" className="meme-img" />
 			</form>
 		</main>
 	);
