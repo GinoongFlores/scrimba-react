db62c63d65bbb41c6197fd4aabb9ecb83e70ac02
GinoongFlores <florespaul161@gmail.com>
2023-07-25 20:41:54 +0800

    feat(App): declarative looping

--- scrimba-box-challenge/src/App.jsx
+++ scrimba-box-challenge/src/App.jsx
@@ -7,33 +7,15 @@ function App() {
 	const [squares, setSquare] = useState(boxes);
 
 	function toggle(id) {
+		// let's make a declarative way to update the state
 		setSquare((prevSquare) => {
-			// create a new array
-			const newSquare = [];
-			// loop over the old array of squares{}
-			// this is an imperative way of updating the state rather than declarative.
-			for (let i = 0; i < prevSquare.length; i++) {
-				// store previous square objects in a variable
-				const currentSquare = prevSquare[i];
-				console.log(currentSquare);
-				// if the current square id is equal to the id passed in the function
-				if (currentSquare.id === id) {
-					// create a new object with the current square
-					const updateSquare = {
-						// get all the properties from the current square object except the on property and set it to the opposite of the current on property
-						...currentSquare,
-						on: !currentSquare.on,
-					};
-					// push the updateSquare to the newSquare array
-					console.log(updateSquare);
-					newSquare.push(updateSquare);
-				} else {
-					// else push the current square to the newSquare array
-					newSquare.push(currentSquare);
-				}
-			}
-			// return the newSquare array to let React render our square state array with the updated square.
-			return newSquare;
+			// return and map over the array of objects to go inside of the array and get the objects. The map method will return a new array of objects with the same of length of the original array without modifying the original array of objects.
+			return prevSquare.map((square) => {
+				// once we are now inside of the object, we want to check if the id of the object matches the id that was passed in as an argument to the function.
+
+				// If it matches, we want to return a new object of squares by spreading them and overwrite the `on` property with the opposite value of what it was before. And if it doesn't match, return the previous square.
+				return square.id === id ? { ...square, on: !square.on } : square;
+			});
 		});
 	}
 
