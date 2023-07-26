3ccdaeeae9d6fdcfbf5b3a80984b9d205bca396d
Christian Paul Flores <florespaul161@gmail.com>
2023-07-26 14:34:13 +0800

    Update App.jsx

--- scrimba-box-challenge/src/App.jsx
+++ scrimba-box-challenge/src/App.jsx
@@ -9,11 +9,13 @@ function App() {
 	function toggle(id) {
 		// let's make a declarative way to update the state
 		setSquare((prevSquare) => {
-			// return and map over the array of objects to go inside of the array and get the objects. The map method will return a new array of objects with the same of length of the original array without modifying the original array of objects.
+			// return and map over the array of objects to go inside the array and get the objects. 
+			// The map method will return a new array of objects  with the same length of the original array without modifying the original array of objects.
 			return prevSquare.map((square) => {
-				// once we are now inside of the object, we want to check if the id of the object matches the id that was passed in as an argument to the function.
+				//Once inside the object, we want to check if the object's id matches the id passed in as an argument to the function.
 
-				// If it matches, we want to return a new object of squares by spreading them and overwrite the `on` property with the opposite value of what it was before. And if it doesn't match, return the previous square.
+				// If it matches, we want to return a new object of squares by spreading them and overwriting the `on` property with the opposite value of what it was before. 
+				// And if it doesn't match, return the previous square.
 				return square.id === id ? { ...square, on: !square.on } : square;
 			});
 		});
