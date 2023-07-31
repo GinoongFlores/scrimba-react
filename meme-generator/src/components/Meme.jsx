0ac0529269c5e963c20c159e8dc11ef7f452e822
Christian Paul Flores <flores.135577@cdo.sti.edu.ph>
2023-07-31 13:49:47 +0800

    Revert "feat(styles & Meme): use grid"
    This reverts commit d2fd81dc42c85c4151d4870cd3da5d102bb8f81d.

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,16 +2,14 @@ import "../index.css";
 
 const Meme = () => {
 	return (
-		<main>
-			<form className="form">
-				<input type="text" placeholder="Top Text" className="form--input" />
-				<input type="text" placeholder="Bottom Text" className="form--input" />
-				<button type="button" className="form--button">
-					Get a new meme image 🖼
-				</button>
+		<div className="meme">
+			<form>
+				<input type="text" placeholder="Top Text" />
+				<input type="text" placeholder="Bottom Text" />
 			</form>
+			<button type="button">Get a new meme image 🖼</button>
 			<img src="/img/memeimg.png" alt="image meme" className="meme-img" />
-		</main>
+		</div>
 	);
 };
 
