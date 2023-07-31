a772a73b8bd236ddaa36fd5097d755ee19ff59a6
Christian Paul Flores <flores.135577@cdo.sti.edu.ph>
2023-07-31 13:49:47 +0800

    Revert "feat(Meme): add useState hook & randomize images"
    This reverts commit 038fefe65f862b5ff0b71fa5d27c63ecbc632646.

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,28 +1,17 @@
 import "../index.css";
-import React from "react";
 import memesData from "../memesData";
 
 // console.log(memesData);
 
 const Meme = () => {
-	const [memeImg, setMemeImg] = React.useState("");
+	// console.log(memesData.data.memes.url);
 
-	// Inline way to do it
-	// const randomeMemeImg =
-	// 	memesData.data.memes[
-	// 		Math.floor(Math.random() * memesData.data.memes.length)
-	// 	].url;
-
-	// Another way to do it to make it more readable
-	const memesArray = memesData.data.memes; // array of memes
-	const randomNumber = Math.floor(Math.random() * memesArray.length) + 1; // random number between 1 and 100
-	let url = memesArray[randomNumber].url; // url of the meme at the random position
-
-	function getMemeImg() {
-		// we can decide not to use a callback function here because we will not be using the previous state of the `memeImg` state which is the empty string.
-		setMemeImg(url);
-
-		console.log(memeImg);
+	function buttonClick() {
+		const randomeMemeImg =
+			memesData.data.memes[
+				Math.floor(Math.random() * memesData.data.memes.length)
+			].url;
+		console.log(randomeMemeImg);
 	}
 
 	return (
@@ -30,10 +19,10 @@ const Meme = () => {
 			<form className="form">
 				<input type="text" placeholder="Top Text" className="form--input" />
 				<input type="text" placeholder="Bottom Text" className="form--input" />
-				<button type="button" onClick={getMemeImg} className="form--button">
+				<button type="button" onClick={buttonClick} className="form--button">
 					Get a new meme image 🖼
 				</button>
-				<img src={memeImg} alt="image meme" className="meme-img" />
+				<img src="/img/memeimg.png" alt="image meme" className="meme-img" />
 			</form>
 		</main>
 	);
