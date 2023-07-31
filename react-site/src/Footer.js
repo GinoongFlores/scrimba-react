35e66ec855cb6d8c6cb1b0413466eb8ca6330773
GinoongFlores <florespaul161@gmail.com>
2023-07-31 16:42:31 +0800

    initialize react-site-facts Project

--- react-site/src/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='footer'>
-            <small> © 2023 Flores development. All rights reserved.</small>
-        </footer>
-    )
-}
-
