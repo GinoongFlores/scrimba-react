dc6ec5f266d1c215a5541f64d92125e17236aa5b
Christian Paul Flores <flores.135577@cdo.sti.edu.ph>
2023-07-31 13:49:47 +0800

    Revert "feat(img & Card): transfer img to /public"
    This reverts commit dfbb65a97c9bfc762483d443c760a5128984af6c.

--- solo-project-2/src/components/Card.jsx
+++ solo-project-2/src/components/Card.jsx
@@ -5,32 +5,31 @@ const Card = (props) => {
 	return (
 		<>
 			<div className="card">
-				<div className="img-div">
-					<img
-						src={`/img/${props.item.imgCountry}`}
-						alt={`${props.item.altCountry}`}
-						className="img-country"
-					/>
-				</div>
+				<img
+					src={`src/assets/img/${props.imgCountry}`}
+					alt={`${props.altCountry}`}
+					className="img-country"
+				/>
 				<div className="card--details">
 					<div className="card--country-view">
 						<p>
-							<img src="/img/location.svg" alt="location" />
-							<span className="country">{props.item.country}</span>{" "}
+							<img src="src/assets/location.svg" alt="location" />
+							<span className="country">{props.country}</span>{" "}
 						</p>
 						<p>
-							<a className="maps" href={`${props.item.googleMapsUrl}`}>
+							<a className="maps" href={`${props.googleMapsUrl}`}>
 								View on Google Maps
 							</a>
 						</p>
 					</div>
-					<h2 className="landmark">{props.item.landmark}</h2>
+					<h2 className="landmark">{props.landmark}</h2>
+
 					<p className="dates">
 						<span className="bold">
-							{props.item.startDate} - {props.item.endDate}
+							{props.startDate} - {props.endDate}
 						</span>
 					</p>
-					<p>{props.item.description}</p>
+					<p>{props.description}</p>
 				</div>
 			</div>
 		</>
