9498ee4fe78976fabceaae78f0c50dc87a760b4d
GinoongFlores <florespaul161@gmail.com>
2023-08-04 18:10:41 +0800

    feat(App): add & delete() from firestore

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -3,6 +3,8 @@ import Sidebar from "./components/Sidebar";
 import { useEffect, useState } from "react";
 import Split from "react-split";
 import { nanoid } from "nanoid";
+import { onSnapshot, addDoc, doc, deleteDoc } from "@firebase/firestore";
+import { notesCollection, db } from "./firebase";
 import "./index.css";
 
 export default function App() {
@@ -12,36 +14,49 @@ export default function App() {
 		this is an example of a "lazy initializer" because we passed a function to useState
 		only when state is first accessed, and not when the component is first rendered.
 	*/
-	const [notes, setNotes] = useState(
-		() => JSON.parse(localStorage.getItem("notes")) || []
-	);
+	const [notes, setNotes] = useState([]);
+
 	const [currentNoteId, setCurrentNoteId] = useState(notes[0]?.id) || "";
 
 	const currentNote =
 		notes.find((note) => note.id === currentNoteId) || notes[0];
-	// sync notes with localStorage
+
+	async function createNewNote() {
+		const newNote = {
+			body: "# Type your markdown note's title here",
+		};
+		// add the newNote to the database by calling the addDoc function with two arguments, the first is the collection reference, and the second is the data object that we want to add to the collection.
+
+		// addDoc returns a promise so we need to use await to wait for the promise to resolve and async the function to use await.
+		const newNoteRef = await addDoc(notesCollection, newNote);
+		setCurrentNoteId(newNoteRef.id);
+	}
 
 	useEffect(() => {
-		// turn the notes array into a stringified JSON object
-		localStorage.setItem("notes", JSON.stringify(notes));
-		// console.log("notes ", notes);
-		// console.log(notes[0].body);
-	}, [notes]); // every time notes changes, save it to localStorage
+		// the onSnapshot takes two arguments, the first is the collection reference, and the second is a callback function whenever there's a change to the "notes" collection.
+
+		// the snapshot holds the data of the collection whenever the collection is updated or changed. The onSnapshot() listener creates a websocket connection to the database to receive the data in real time. And we don't want to hang the listener when the component unmounts or whenever the tab close which causes memory leak when there's a listener listening in the background. So we need to return a function from the useEffect hook to clean up the listener.
+		const unsubscribe = onSnapshot(notesCollection, (snapshot) => {
+			// Sync up local notes array with the snapshot data
+			const noteArr = snapshot.docs.map((doc) => ({
+				// return all the data object from the document.
+				...doc.data(),
+				// firestore doesn't store the id in the data object, the document has its own id property. So we need to add the id property to the data object.
+				id: doc.id,
+			}));
+			// set the notes array to the noteArr where it contains all the data from the documents in the collection.
+			setNotes(noteArr);
+		});
+
+		// return a function to clean up the listener. Because the unsubscribe is a function we can call it directly without creating a new function.
+		return unsubscribe;
+	}, []);
 
 	// localStorage.clear();
 
-	function createNewNote() {
-		const newNote = {
-			id: nanoid(),
-			body: "# Type your markdown note's title here",
-		};
-		setNotes((prevNotes) => [newNote, ...prevNotes]);
-		setCurrentNoteId(newNote.id);
-	}
-
 	function updateNote(text) {
 		// trying to rearrange the most recently modified note to be at the top of the sidebar
-		setNotes((OldNotes) => {
+		setNotes((oldNotes) => {
 			// create a new empty array
 			// Loop over the origin array
 			// if the id matches
@@ -49,9 +64,9 @@ export default function App() {
 			// else
 			// push the old note to the end of the new array
 			// return the new array
-			if (OldNotes.length > 0) {
+			if (oldNotes.length > 0) {
 				const newArray = [];
-				OldNotes.map((note) => {
+				oldNotes.map((note) => {
 					if (note.id === currentNoteId) {
 						newArray.unshift({ ...note, body: text });
 					} else {
@@ -62,8 +77,8 @@ export default function App() {
 			}
 
 			// imperative way of making the recent note at the top of the sidebar
-			// for (let i = 0; i < OldNotes.length; i++) {
-			// 	const oldNote = OldNotes[i];
+			// for (let i = 0; i < oldNotes.length; i++) {
+			// 	const oldNote = oldNotes[i];
 			// 	if (oldNote.id === currentNoteId) {
 			// 		newArray.unshift({ ...oldNote, body: text });
 			// 	} else {
@@ -85,13 +100,10 @@ export default function App() {
 	 *    can be passed in during the onClick event handler
 	 */
 
-	function deleteNote(event, noteId) {
-		event.stopPropagation();
-
-		/* 
-			filter or look into notes array and if the note.id is not equal to the noteId or is not the note that we clicked on which results to true, then we want to keep it in the array. If note.id is equal to the noteId or is the note that we clicked on, then we want to remove it from the array.
-		*/
-		setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+	// the doc() helps us to access a single document in the collection. The doc() takes two arguments, the first is the collection reference, and the second is the name of the collection that were trying to delete from the database. And the third argument is the id of the document that we want to delete.
+	async function deleteNote(noteId) {
+		const docRef = doc(db, "notes", noteId);
+		await deleteDoc(docRef);
 	}
 
 	return (
@@ -105,9 +117,8 @@ export default function App() {
 						newNote={createNewNote}
 						deleteNote={deleteNote}
 					/>
-					{currentNoteId && notes.length > 0 && (
-						<Editor currentNote={currentNote} updateNote={updateNote} />
-					)}
+
+					<Editor currentNote={currentNote} updateNote={updateNote} />
 				</Split>
 			) : (
 				<div className="no-notes">
